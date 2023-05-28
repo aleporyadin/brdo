a22463e887b2a3e7f6d80d683e7328c572becaf3
Oleksandr Poriadin <ale.poryadin@gmail.com>
2023-05-28 17:12:12 +0300

    Completed v0.1

--- src/main/java/com/poriadin/brdo/services/AccountService.java
+++ src/main/java/com/poriadin/brdo/services/AccountService.java
@@ -3,13 +3,12 @@ package com.poriadin.brdo.services;
 import com.poriadin.brdo.dto.AccountDto;
 import com.poriadin.brdo.entities.Account;
 import com.poriadin.brdo.entities.Role;
-import com.poriadin.brdo.repositories.RoleRepository;
 import com.poriadin.brdo.repositories.AccountRepository;
+import com.poriadin.brdo.repositories.RoleRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class AccountService {
@@ -40,18 +39,6 @@ public class AccountService {
 
     public Account findByUsername(String username) {
         return accountRepository.findByUsername(username);
-    }
-
-    public List<AccountDto> findAllUsers() {
-        List<Account> accounts = accountRepository.findAll();
-        return accounts.stream().map(this::convertEntityToDto)
-                .collect(Collectors.toList());
-    }
-
-    private AccountDto convertEntityToDto(Account user) {
-        AccountDto userDto = new AccountDto();
-        userDto.setUsername(user.getUsername());
-        return userDto;
     }
 
     private Role checkRoleExist() {
